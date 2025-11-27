7981d67c9e2e8f3ce3611d4ab3fd0277a8b8e50e
8imai8 <luyejinjing442@gmail.com>
2025-11-27 19:26:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
 
-      base: "/<vibematch>/",
+      base: "/vibematch/",
         
       plugins: [react()],
       define: {
